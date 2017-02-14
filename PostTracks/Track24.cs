9e77c49405cd7f00f346a92ba45266a2460b6c84
Vlad S <4ton106@mail.ru>
2017-02-14 22:52:04 +0400

    PostTracks

---
+++ PostTracks/Track24.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostTracks
+{
+    public class Track24
+    {
+        private string apiKey = "67a4ca1f23eff5627683b9c142db69de";
+        private string domain = "excel.vba.demo";
+        Random rnd = new Random();
+        public string getTrackCodeInfo(string currentTrackCode, string apiKey, string domain)
+        {
+            string cUrl = @"https://track24.ru/api/tracking.json.php?apiKey=" + apiKey + "&domain=" + domain + "&code=" + currentTrackCode + "&rnd=" + rnd.Next(10000,99999).ToString();
+            return "Track24.getTrackCodeInfo() _  return";
+        }
+    }
+}

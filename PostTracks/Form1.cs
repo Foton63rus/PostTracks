1dfa5ea701fa3fab89ea6e2f331d0849b445ca39
Vlad S <4ton106@mail.ru>
2017-03-02 00:19:34 +0400

    #10 Запущен перезаписанный класс track24.

--- PostTracks/Form1.cs
+++ PostTracks/Form1.cs
@@ -19,24 +19,14 @@ namespace PostTracks
             //https://www.aftership.com/docs/api/4/overview
             // TRACK24
             //'error' : 'TooManyRequests'
-            //В данный момент, стандартный лимит : 
-            //10 запросов / сек (600 запросов / мин), 
-            //1000 трек-кодов в сутки (на аккаунт / домен)
-            //трек-код в формате AZ#########AZ
 
             InitializeComponent();
-            //Tracker_track24.getSingleTrackInfo("RB166350968SG");
 
+            //Tracker_track24.getSingleTrackInfo("RB166350968SG"); //"RF499423428CN"
             excel = new ExcelIO("");
             Tracker_track24.getManyTrackInfo(excel.LoadedTracks);
             excel.track24dataToExcel();
 
-            //
-            //excel.postDataToExcel( (IPostTrackInfo) );
-            //excel.ExcelAppQuit();
-            //Track24 tr24 = new Track24("RF499423428CN"); //RB166350968SG
-
-            //MessageBox.Show(tr24.ToString());
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
